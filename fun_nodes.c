80cde3d58310d8ca99b96d459b43952cc5ab119f
Noeuclides <euclidesnoeuclides@gmail.com>
2019-05-17 04:11:13 +0000

    push check number

--- fun_nodes.c
+++ fun_nodes.c
@@ -11,15 +11,18 @@ void node_push(stack_t **stack, unsigned int line_number)
 	stack_t *node;
 	int np;
 
-	(void)line_number;
-
 	node = malloc(sizeof(stack_t));
 	if (node == NULL)
 	{
 		printf(ERROR_MALLOC);
 		exit(EXIT_FAILURE);
 	}
-
+        if (number == NULL)
+        {
+                dprintf(STDERR_FILENO, "L%i: usage: push integer", line_number);
+                free_list(*stack);
+                exit(EXIT_FAILURE);
+        }
 
 	np = atoi(number);
 	node->n = np;
