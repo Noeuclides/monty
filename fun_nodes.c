9100ddcccc18cdbea98c8d152b75a67343c70389
naxus1 <naxus1@gmail.com>
2019-05-17 18:16:43 +0000

    Modified pop

--- fun_nodes.c
+++ fun_nodes.c
@@ -60,7 +60,7 @@ void pall(stack_t **stack, unsigned int line_number)
  * @stack: head stack
  * @line_number: line number
  */
-void pop(stack_t **stack, unsigned int line_number)
+void node_pop(stack_t **stack, unsigned int line_number)
 {
 	stack_t *aux;
 
